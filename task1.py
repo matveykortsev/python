980d2a46a2c53b14d18e829f92713a7f507fe159
matveykortsev <63moty@gmail.com>
2021-02-02 14:24:32 +0400

    Initial commit

---
+++ task1.py
@@ -0,0 +1,11 @@
+number = 1
+string = 'Hello world'
+
+print('Заданное число', number)
+print('Заданная строка', string)
+
+number = int(input('Введите новое число '))
+string = input('Введите новую строку ')
+
+print(number)
+print (string)
